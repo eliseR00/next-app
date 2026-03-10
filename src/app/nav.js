740a2ed9ca69a7a539898950a803633fa97bb77e
eliseR00 <eroberts1029@hotmail.com>
2026-03-10 16:59:47 -0400

    init proj

---
+++ src/app/nav.js
@@ -0,0 +1,11 @@
+const Nav = () => {
+    return (
+        <nav className="nav">
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/about">About</a></li>
+            </ul>
+        </nav>
+    );
+};
+export default Nav;
